fdf5385d5892c17b4fb03f88dd837e8df0a9f7fe
Alexis Roux <roux.alexis25@gmail.com>
2016-06-02 14:07:00 +0200

    Ajout de maj + modification main

--- gitTestTrio/src/presentation/Main.java
+++ gitTestTrio/src/presentation/Main.java
@@ -1,5 +1,6 @@
 package presentation;
 
+import metier.Maj;
 import metier.Prefix;
 import metier.Sufix;
 
@@ -11,12 +12,10 @@ public class Main {
 		
 		String phraseSufix = Sufix.transform(phrase);
 		String phrasePrefix= Prefix.transform(phrase);
+		String phraseMaj = Maj.transform(phrase);
 		System.out.println("Phrase de base : "+phrase);
 		System.out.println("Phrase avec sufixe : "+phraseSufix);
 		System.out.println("Phrase avec préfixe :"+phrasePrefix);
-		
+		System.out.println("Phrase en majuscules : "+phraseMaj);
 	}
-	
-
-
 }
